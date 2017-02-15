ada4dd78759d903a0709d2aed0612a3c04d71685
Barry Oliver <i31770@gmail.com>
2017-02-15 16:34:59 -0500

    output goes to table.html

--- linearRegression.py
+++ linearRegression.py
@@ -1,55 +1,9 @@
-
-from pylab import scatter, show, title, xlabel, ylabel, plot, contour
-from numpy import loadtxt, zeros, ones, array, linspace, logspace
-
 import csv
+import operator
+from tabulate import tabulate
 
-# with open('housing_training.csv') as trainingFile:
-#     readTraining = csv.reader(trainingFile, delimiter=',')
-data = loadtxt('housing_training.csv', delimiter=',')
-
-scatter(data[:, 0], data[:, 1], marker='o', c='b')
-title('Profits distribution')
-xlabel('Population of City in 10,000s')
-ylabel('Profit in $10,000s')
-show()
-
-with open('housing_test.csv') as testFile:
-    readTest = csv.reader(testFile, delimiter=',')
-    for testRow in readTest:
-        print(testRow[0], testRow[1])
-
-
-def feature_normalize(X):
-    mean_r = []
-    std_r = []
-
-    X_norm = X
-
-    n_c = X.shape[1]
-    for i in range(n_c):
-        m = mean(X[:, i])
-        s = std(X[:, i])
-        mean_r.append(m)
-        std_r.append(s)
-        X_norm[:, i] = (X_norm[:, i] - m) / s
-
-    return X_norm, mean_r, std_r
-
-
-def compute_cost(X, y, theta):
-    # Number of training samples
-    m = y.size
-
-    predictions = X.dot(theta)
-
-    sqErrors = (predictions - y)
-
-    J = (1.0 / (2 * m)) * sqErrors.T.dot(sqErrors)
-
-    return J
-
-
-
-# training data for 1.
-# test data for 2,3,4
+with open("housing_training.csv") as data:
+    datareader = csv.reader(data, delimiter=',')
+    with open('table.html', 'a') as out:
+        out.write(tabulate(datareader, headers="keys", tablefmt="html"))
+    # print(tabulate(datareader, headers="keys", tablefmt="html"))
