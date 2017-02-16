1b623224c8d93b8a4f9ad7c1eb326f249b4de11c
Barry Oliver <i31770@gmail.com>
2017-02-16 17:17:10 -0500

    Loading training and test data into array is working. (numpy) Equation almost working, but is not complete.

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,5 @@
 import csv
-import operator
+import numpy
 import os
 from tabulate import tabulate
 
@@ -9,20 +9,21 @@ try:
 except OSError:
     pass
 
-sumX = 0
 
-with open('housing_training.csv', 'r') as training:
-    sumY = 0
-    data2 = csv.reader(training, delimiter=',')
-    rowcount = sum(1 for row in data2)
-    for row in data2:
-        sumY += float(row[13])
-        x = row[0:13]
-        print("")
+def getcolumnsums(matrix):
+    columns = matrix.sum(axis=0)
+    # number of rows is 300
+    return columns / 300
 
-# print(rowcount)
+
 with open("housing_training.csv") as data:
-    datareader = csv.reader(data, delimiter=',')
-    with open('table.html', 'a') as out:
-        out.write("<style>td{border: 1px solid black;padding: 5px;}</style>")
-        out.write(tabulate(datareader, headers="keys", tablefmt="html"))
+    b = getcolumnsums(numpy.loadtxt(open("housing_training.csv", "r"), delimiter=","))
+    X = numpy.loadtxt(open("housing_test.csv", "r"), delimiter=",")
+    y = X * b
+    print(X)
+    # print(b)
+
+    # y =Xb + e
+    # with open('table.html', 'a') as out:
+    #     out.write("<style>td{border: 1px solid black;padding: 5px;}</style>")
+    #     out.write(tabulate(trainingdata, headers="keys", tablefmt="html"))
