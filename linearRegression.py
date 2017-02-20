13ea645b07c33fa834b5d6775ad4e30297c9e924
Barry Oliver <i31770@gmail.com>
2017-02-19 23:39:31 -0500

    Mostly working.

--- linearRegression.py
+++ linearRegression.py
@@ -1,9 +1,8 @@
-from statistics import mean
-import numpy
-import matplotlib.pyplot as plt
-import random
-from matplotlib import style
-from tabulate import tabulate
+# Barry Oliver
+# 2/19/2017
+
+import matplotlib.pyplot as plt1
+import matplotlib.pyplot as plt2
 from pylab import *
 
 style.use('ggplot')
@@ -12,11 +11,9 @@ style.use('ggplot')
 training_data = loadtxt(open("housing_training.csv", "r"), delimiter=",")
 
 # Remove empty rows
-r = 0
 for i in training_data:
     if len(i) == 0:
         training_data.remove(i)
-    r += 1
 
 # Test data array
 test_data = loadtxt(open("housing_test.csv", "r"), delimiter=",")
@@ -25,7 +22,6 @@ test_data = loadtxt(open("housing_test.csv", "r"), delimiter=",")
 for i in training_data:
     if len(i) == 0:
         training_data.remove(i)
-    r += 1
 
 # train the model
 x = []
@@ -49,53 +45,83 @@ x_xt = np.dot(xt, x)
 x_inverse = inv(x_xt)
 xt_y = np.dot(xt, y)
 coefficients = np.dot(xt_y, x_inverse)
+print("Coefficients")
 print(coefficients)
+print("-"*60)
+
+# Predict y values for test_data
+# xvalues from test data
+x_test = []
+for i in test_data:
+    x_test.append(i[:len(i) - 1])
+
+x_test = np.array(x_test, dtype=float)
+# y values from test data
+y_test = []
+for i in test_data:
+    y_test.append(i[len(i) - 1])
+y_test = np.array(y_test, dtype=float)
 
 
-# Returns the slope
-def getslope(x, y):
-    # y = mx + b
-    slope = (((mean(x) * mean(y)) - mean(x * y)) / ((mean(x) * mean(x)) - mean(x * x)))
-    return slope
+# multiply x value per row by coefficient and add to get y
+def predict_y(z, xvalues):
+    pred_y = []
+    y_intercept_coef = z[0]
+    for i in xvalues:
+        temp_y = y_intercept_coef
+        index = 0
+        for j in z[1:]:
+            temp_calc = j * i[index]
+            temp_y += temp_calc
+            index += 1
+        pred_y.append(temp_y)
+    return pred_y
 
 
-# Returns the intercept
-def getintercept(x, y, slope):
-    intercept = mean(y) - slope * mean(x)
-    return intercept
+y_prediction = predict_y(coefficients, x_test)
+index = 0
 
+x3 = []
+y3 = []
+index = 0
+while index <= 50:
+    x3.append(index)
+    y3.append(index)
+    index += 1
 
-slope_training = getslope(xt, y)
-intercept_training = getintercept(x, y, slope_training)
-
-
-def regress_line(x):
-    regression_line = []
-    for i in x:
-        regression_line.append((slope_training * i) + intercept_training)
-    return regression_line
-
-
-testx = test_data[:13]
-testxsum = testx.sum(axis=0) / 206
-testy = test_data[13]
-line_test = regress_line(testxsum)
-print(line_test)
-plt.figure()
-plt.scatter(testxsum, testy, color='green', label='Test Data Points')
-plt.plot(testxsum, line_test, color='blue', label='Regression Line')
+# Plot showing prediction vs ground truth
+plt1.figure()
+plt1.scatter(y_prediction, y_test, color='green', label='Test Data Points')
+plt1.plot(x3, y3, "b")
+plt1.ylabel("Test data")
+plt1.xlabel("Predicted values")
 plt.legend()
-plt.show()
+plt1.show()
+
+# diff of prediction vs ground truth
+ind = []
+diffs = []
+count = 0
+for i in y_test:
+    ind.append(count)
+    diffs.append(y_prediction[count] - i)
+    count += 1
+
+plt.figure()
+plt2.scatter(ind, diffs, color='orange', label='Difference')
+plt2.ylabel("Difference")
+plt2.xlabel("Sample")
+plt2.show()
 
 # Residual Sum of Squares
-# rss = y' * y - b' * x' * y
 yt = y.transpose()
 bt = coefficients.transpose()
-#  yt * y
+#  y_transpose * y
 y_yt = np.dot(yt, y)
-# xt * y
+# x_transpose * y
 y_xt = np.dot(xt, y)
-# bt * (y*xt)
+# b_transpose * (y*x_transpose)
 b_y_xt = np.dot(bt, y_xt)
 rss = y_yt - b_y_xt
+print("Residual Sum of Squares")
 print(rss)
