65b3e8b67e5e8c7d1a46078fd40055d20f4cba56
Barry Oliver <i31770@gmail.com>
2017-02-19 22:25:43 -0500

    rss works. coefficients are working too.

--- linearRegression.py
+++ linearRegression.py
@@ -8,35 +8,65 @@ from pylab import *
 
 style.use('ggplot')
 
-# Training data
+# Training data array
 training_data = loadtxt(open("housing_training.csv", "r"), delimiter=",")
-training_data_transpose = training_data.transpose()
-x_training = training_data_transpose[0:13].sum(axis=0)
-y_training = training_data_transpose[13]
 
-# Test data
+# Remove empty rows
+r = 0
+for i in training_data:
+    if len(i) == 0:
+        training_data.remove(i)
+    r += 1
+
+# Test data array
 test_data = loadtxt(open("housing_test.csv", "r"), delimiter=",")
-test_data_transpose = test_data.transpose()
-x_test = test_data_transpose[0:13].sum(axis=0)
-y_test = test_data_transpose[13]
 
-# Table of the coefficients in the linear regression model
-print(tabulate(mat(x_training), headers="keys", tablefmt="plain"))
+# Remove empty rows
+for i in training_data:
+    if len(i) == 0:
+        training_data.remove(i)
+    r += 1
+
+# train the model
+x = []
+# z is used as a temporary array
+z = []
+for i in training_data:
+    z = [1]
+    for j in i[:13]:
+        z.append(j)
+    x.append(z)
+
+y = []
+for i in training_data:
+    y.append(i[13])
+
+# numpy array with type float
+x = np.array(x, dtype=float)
+y = np.array(y, dtype=float)
+xt = x.transpose()
+x_xt = np.dot(xt, x)
+x_inverse = inv(x_xt)
+xt_y = np.dot(xt, y)
+coefficients = np.dot(xt_y, x_inverse)
+print(coefficients)
 
 
+# Returns the slope
 def getslope(x, y):
     # y = mx + b
     slope = (((mean(x) * mean(y)) - mean(x * y)) / ((mean(x) * mean(x)) - mean(x * x)))
     return slope
 
 
+# Returns the intercept
 def getintercept(x, y, slope):
     intercept = mean(y) - slope * mean(x)
     return intercept
 
 
-slope_training = getslope(x_training, y_training)
-intercept_training = getintercept(x_training, y_training, slope_training)
+slope_training = getslope(xt, y)
+intercept_training = getintercept(x, y, slope_training)
 
 
 def regress_line(x):
@@ -46,24 +76,26 @@ def regress_line(x):
     return regression_line
 
 
-line_test = regress_line(x_test)
-
-predict_x = numpy.array(x_training)
-predict_y = numpy.array([(slope_training * j) + intercept_training for j in predict_x])
-
+testx = test_data[:13]
+testxsum = testx.sum(axis=0) / 206
+testy = test_data[13]
+line_test = regress_line(testxsum)
+print(line_test)
 plt.figure()
-plt.scatter(x_test, y_test, color='green', label=str(len(x_test)) + ' Test Data Points')
-plt.plot(x_test, line_test, color='blue', label='Regression Line')
+plt.scatter(testxsum, testy, color='green', label='Test Data Points')
+plt.plot(testxsum, line_test, color='blue', label='Regression Line')
 plt.legend()
 plt.show()
-print(sum(y_test))
-print(sum(x_test))
 
-# x is a vector
-# X is a matrix
-# y is last column in housing_test.csv
-# b is the optimal coefficients of the model
-
-# RSS is not correct
-rss = sum((y_test - (test_data_transpose/206 * x_test/206))) ** 2
+# Residual Sum of Squares
+# rss = y' * y - b' * x' * y
+yt = y.transpose()
+bt = coefficients.transpose()
+#  yt * y
+y_yt = np.dot(yt, y)
+# xt * y
+y_xt = np.dot(xt, y)
+# bt * (y*xt)
+b_y_xt = np.dot(bt, y_xt)
+rss = y_yt - b_y_xt
 print(rss)
